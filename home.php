b32ba5aebb58bb66ccbd7f0b4876d10704f7399d
satishkkk <satishkathiriya99@gmail.com>
2016-07-23 18:48:56 +0530

    login done signup done multiple signup done

---
+++ home.php
@@ -0,0 +1,52 @@
+<?php
+	$error="";
+	
+	$f_name=$_POST["USername"];
+	if($f_name==""||count($u_name)==0)
+	{
+	$error=$error."enter ur name"."<br>";
+	}
+	//echo $u_name."<br/>";
+
+	$l_name=$_POST["Password"];
+	if($l_name==""||count($u_pass)==0)
+	{
+	$error=$error."enter ur passsword"."<br>";
+	}
+
+	$email=$_POST["USername"];
+	if($email==""||count($u_name)==0)
+	{
+	$error=$error."enter ur name"."<br>";
+	}
+
+	$password=$_POST["USername"];
+	if($password==""||count($u_name)==0)
+	{
+	$error=$error."enter ur name"."<br>";
+	}
+
+	$c_pass=$_POST["USername"];
+	if($c_pass==""||count($u_name)==0)
+	{
+	$error=$error."enter ur name"."<br>";
+	}
+	//echo $u_pass."<br/>";
+
+	$number=$_POST["USername"];
+	if($number==""||count($u_name)==0)
+	{
+	$error=$error."enter ur name"."<br>";
+	}
+
+	if(strlen($error)!==0)
+	{
+		echo $error;
+	}
+	else
+	{
+		echo "Form submited successfully";
+	}
+	
+?>
+
