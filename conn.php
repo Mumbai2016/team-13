f2bd3484f34e8be3896c91eda5381bea4b9ec825
ayushigaur <ayushixyz26@gmail.com>
2016-07-23 21:47:12 +0530

    login reference file

--- conn.php
+++ conn.php
@@ -2,11 +2,10 @@
 $dbHost = 'localhost';
 $dbUser = 'root';
 $dbPass = '';
-$dbName = 'test';
+$dbName = 'dreamd';
 $dbConn = mysql_connect ($dbHost, $dbUser, $dbPass) 
 or 
 die ('MySQL connect failed. ' . mysql_error());
 mysql_select_db($dbName)  or 
 die('Cannot select database. ' . mysql_error());
-echo 'connected';
 ?>
